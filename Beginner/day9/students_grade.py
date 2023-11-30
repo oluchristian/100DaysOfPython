d8afe6d7bf2870e6d1da43781b5805a12cee1fd8
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-11-30 01:51:14 +0100

    dictionary in list

---
+++ Beginner/day9/students_grade.py
@@ -0,0 +1,26 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+student_grades = {}
+
+for keys in student_scores:
+  if student_scores[keys] >= 91 and student_scores[keys] <= 100:
+    student_grades[keys] = "Outstanding"
+  elif student_scores[keys] >= 81 and student_scores[keys] <= 90:
+    student_grades[keys] = "Exceeds Expectations"
+  elif student_scores[keys] >= 71 and student_scores[keys] <= 80:
+    student_grades[keys] = "Acceptable"
+  elif student_scores[keys] <= 70:
+    student_grades[keys] = "Fail"
+
+print(student_grades)
+
+
+
+
+
